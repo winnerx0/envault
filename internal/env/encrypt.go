ef94266f91981fc53ee02d1bba63a1fc078ac95d
winnerx0 <eragbonikpeya@gmail.com>
2026-03-22 09:58:59 +0100

    Refactor encryption to use project root and clean up file handling

--- internal/env/encrypt.go
+++ internal/env/encrypt.go
@@ -4,13 +4,13 @@ import (
 	"crypto/aes"
 	"crypto/cipher"
 	"crypto/rand"
-	"fmt"
 	"io"
 	"io/fs"
 	"os"
 	"path/filepath"
 	"regexp"
 
+	"github.com/winnerx0/envault/internal/config"
 	"golang.org/x/crypto/argon2"
 )
 
@@ -18,93 +18,66 @@ func DeriveKey(passphrase string, salt []byte) []byte {
 	return argon2.IDKey([]byte(passphrase), salt, 3, 32*1024, 4, 32)
 }
 
-func EncryptFile(inputfile string, outputfile string, passphase string) error {
+func EncryptFile(passphrase string) error {
 
-	dir, err := os.Getwd()
-
+	dir, err := config.FindProjectRoot()
 	if err != nil {
 		return err
 	}
 
-	regex := regexp.MustCompile(".env.*")
+	regex := regexp.MustCompile(`^\.env.*`)
 
 	err = filepath.WalkDir(dir, func(path string, d fs.DirEntry, err error) error {
 
 		if err != nil {
+			return err
+		}
+
+		if d.IsDir() {
 			return nil
 		}
 
-		if path == dir {
-			return nil
-		}
-
-		
 		if !regex.MatchString(d.Name()) {
 			return nil
 		}
-		
-		fmt.Println("name", path)
 
 		plaintext, err := os.ReadFile(path)
-
 		if err != nil {
 			return err
 		}
 
 		salt := make([]byte, 16)
+		io.ReadFull(rand.Reader, salt)
 
-		if _, err := io.ReadFull(rand.Reader, salt); err != nil {
-			return err
-		}
-
-		key := DeriveKey(passphase, salt)
+		key := DeriveKey(passphrase, salt)
 
 		block, err := aes.NewCipher(key)
-
 		if err != nil {
 			return err
 		}
 
 		gcm, err := cipher.NewGCM(block)
-
 		if err != nil {
 			return err
 		}
 
 		nonce := make([]byte, gcm.NonceSize())
-
-		if _, err := io.ReadFull(rand.Reader, nonce); err != nil {
-			return err
-		}
+		io.ReadFull(rand.Reader, nonce)
 
 		ciphertext := gcm.Seal(nil, nonce, plaintext, nil)
 
-		outfile, err := os.Create(path + ".enc")
-
+		outfile, err := os.OpenFile(path+".enc", os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0600)
 		if err != nil {
 			return err
 		}
 
-		defer outfile.Close()
+		outfile.Write(salt)
+		outfile.Write(nonce)
+		outfile.Write(ciphertext)
 
-		if _, err := outfile.Write(salt); err != nil {
-			return err
-		}
-		if _, err := outfile.Write(nonce); err != nil {
-			return err
-		}
-		if _, err := outfile.Write(ciphertext); err != nil {
-			return err
-		}
+		outfile.Close()
 
 		return nil
-
 	})
-
-	if err != nil {
-		return err
-	}
-
-	return nil
-
+	return err
 }
