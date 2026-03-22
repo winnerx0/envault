e8bad6ac4eae398e618e2359280cb8f3814e3b78
winnerx0 <eragbonikpeya@gmail.com>
2026-03-22 14:23:30 +0100

    Move GitHub token and repo creation to login command
    Login now accepts --token flag, creates a private GitHub repo,
    and saves token/passphrase/repo to ~/.envault/config.yaml.

--- app/login.go
+++ app/login.go
@@ -6,19 +6,17 @@ import (
 
 	"github.com/spf13/cobra"
 	"github.com/winnerx0/envault/internal/config"
-	"github.com/winnerx0/envault/internal/env"
+	"github.com/winnerx0/envault/internal/global"
 )
+
+var password string
+var token string
 
 var loginCmd = &cobra.Command{
 	Use:   "login",
 	Short: "Login to envault",
 	Long:  "Login to envault",
 	RunE: func(cmd *cobra.Command, args []string) error {
-		password, err := cmd.Flags().GetString("password")
-		
-		if err != nil {
-			return err
-		}
 
 		root, err := config.FindProjectRoot()
 		if err != nil {
@@ -26,13 +24,30 @@ var loginCmd = &cobra.Command{
 		}
 		fmt.Println("Project root:", filepath.Base(root))
 
-		env.EncryptFile(password)
+		if token != "" {
+			gcfg := &global.GlobalConfig{Token: token, PassPhrase: password}
+
+			// Create a private repo if one doesn't exist yet
+			repoName := randomString()
+			owner, err := createRepo(token, repoName)
+			if err != nil {
+				return err
+			}
+			gcfg.Repo = owner + "/" + repoName
+			fmt.Println("Created private backup repo:", gcfg.Repo)
+
+			if err := global.Save(gcfg); err != nil {
+				return err
+			}
+			fmt.Println("Config saved to ~/.envault/config.yaml")
+		}
 		return nil
 	},
 }
 
-func init(){
-
-	loginCmd.Flags().String("password", "", "Create a password for envault")
+func init() {
+	loginCmd.Flags().StringVarP(&password, "password", "p", "", "Create a password for envault")
+	loginCmd.MarkFlagRequired("password")
+	loginCmd.Flags().StringVarP(&token, "token", "t", "", "GitHub personal access token for backups")
 	rootCmd.AddCommand(loginCmd)
 }
