208cfeefafe761dc61a1c3248a8dfd754f08074d
winnerx0 <eragbonikpeya@gmail.com>
2026-03-22 14:26:32 +0100

    Remove unneeded comments in main file

--- cmd/envault/main.go
+++ cmd/envault/main.go
@@ -10,32 +10,6 @@ import (
 
 func main() {
 
-	// err := utils.OpenBrowser("http://localhost:8080")
-
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-
-	// passphrase := "super-secret-passphrase" // do NOT hardcode in production
-	// inputFile := ".env"
-	// outputFile := ".env.enc"
-
-	// if err := env.EncryptFile(inputFile, outputFile, passphrase); err != nil {
-	// 	fmt.Println("Error:", err)
-	// } else {
-	// 	fmt.Println("File encrypted to", outputFile)
-	// }
-
-	// bytes, err := os.ReadFile("./.env.enc")
-
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-
-	// if err := env.DencryptFile(bytes, passphrase); err != nil {
-	// 	fmt.Println("Error:", err)
-	// }
-
 	app.Execute()
 
 }
